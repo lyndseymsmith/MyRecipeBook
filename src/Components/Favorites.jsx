d3275931dcdaadaee4d9d5a1373f0e06cde3e871
Lyndsey <lyndseymsmith44@gmail.com>
2025-04-17 20:45:35 -0400

    Added ability to add favorite recipes to the Favorites page, and added Log out button

--- src/Components/Favorites.jsx
+++ src/Components/Favorites.jsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
+import RecipeCard from "./RecipeCard";
+import { useNavigate } from "react-router-dom";
 
 function Favorites() {
+  const [favorites, setFavorites] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(storedFavorites);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token")
+    navigate("/login")
+  }
+
+  if (favorites.length === 0) {
+    return (
+    <div>
+      <button onClick={handleLogout}>Log Out</button>
+      <h2>No favorites found.</h2>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Favorites</h1>
-      <p>This is the favorites page.</p>
+      <button onClick={handleLogout}>Log Out</button>
+      <div className="recipes-container">
+        {favorites.map((r) => (
+          <RecipeCard
+            key={r.id}
+            id={r.id}
+            name={r.name}
+            photo={r.photo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
